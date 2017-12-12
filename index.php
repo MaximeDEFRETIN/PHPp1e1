face26ccc32f98f40fb7152b8e309e5ce969759f
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-12 16:08:40 +0100

    Correction

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-$nom = "Dovahkiin";
-echo $nom;
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css" />
+    <title>Partie 1 : exercice 1</title>
+  </head>
+  <body>
+  <?php
+  $nom = 'Dovahkiin';
+  echo $nom;
+  ?>
+  </body>
+</html>
